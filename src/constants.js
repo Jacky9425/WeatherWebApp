52a2e4052433cd2efa2cc4c30c867a16dc0a35aa
Jacky <jackylim.lim@gmail.com>
2022-06-19 14:17:22 +0800

    API url changes

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,10 @@ const format = "YYYY-MM-DD hh:mm:ss A";
 
 const apiKey = "0912e96288353db75b1127aa3fb4fe6a";
 
-const GetGeocoding = (city, country) => {
+const GetWeather = (city, country) => {
   return new Promise((resolve, reject) => {
     fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&appid=${apiKey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`
     )
       .then((response) =>
         response.status === 200 ? response.json() : reject(response)
@@ -15,17 +15,4 @@ const GetGeocoding = (city, country) => {
   });
 };
 
-const GetWeather = (lat, lon) => {
-  return new Promise((resolve, reject) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
-    )
-      .then((response) =>
-        response.status === 200 ? response.json() : reject(response)
-      )
-      .then((res) => resolve(res))
-      .catch((e) => reject(e));
-  });
-};
-
-export { format, GetGeocoding, GetWeather };
+export { format, GetWeather };
--- src/contexts/appContext.js
+++ src/contexts/appContext.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import moment from "moment";
 import { message } from "antd";
-import { format, GetGeocoding, GetWeather } from "../constants";
+import { format, GetWeather } from "../constants";
 
 const AppContext = React.createContext(null);
 
@@ -31,33 +31,26 @@ function AppContextProvider(props) {
   const fetchWeather = (city = "ipoh", country = "malaysia", obj = {}) => {
     setLoading(true);
 
-    // get latitude and longitude of city
-    GetGeocoding(city, country)
+    GetWeather(city, country)
       .then((res) => {
-        const { lat, lon, name, country } = res[0];
+        const { name } = res;
+
+        setLoading(false);
+        setWeatherError(false);
 
         if (Object.keys(obj).length > 0) {
-          addHistory({
-            ...obj,
-            lat,
-            lon,
-          });
+          addHistory(obj);
         }
 
-        // pass the value to fetch weather
-        GetWeather(lat, lon).then((weather_res) => {
-          setLoading(false);
-          setWeatherError(false);
-          setWeatherData({
-            name,
-            country,
-            humidity: weather_res.main.humidity,
-            temp_min: weather_res.main.temp_min,
-            temp_max: weather_res.main.temp_max,
-            main: weather_res.weather[0].main,
-            description: weather_res.weather[0].description,
-            time: moment().format(format),
-          });
+        setWeatherData({
+          name,
+          country: res.sys.country,
+          humidity: res.main.humidity,
+          temp_min: res.main.temp_min,
+          temp_max: res.main.temp_max,
+          main: res.weather[0].main,
+          description: res.weather[0].description,
+          time: moment().format(format),
         });
       })
       .catch((e) => {
